47fe50e927b8d41cd03860ef9e18540fffaef175
12-12-DO4 <mustafayasser031212@gmail.com>
2023-07-14 23:27:58 +0300

    notes of week3

--- meta_web/back/sql_notes.sql
+++ meta_web/back/sql_notes.sql
@@ -21,16 +21,17 @@
     INSERT INTO table1_name (col1, col2) (SELECT (col1, col2) FROM table2_name);
     -- 2) read
     SELECT col_name FROM table_name
+    SELECT col_name FROM table_name WHERE cond;
     SELECT col1_name, col2_name FROM table_name
     SELECT col1_name + col2_name FROM table_name
     SELECT col1_name FROM table_name WHERE col2_name = val
     SELECT col1_name / 2 FROM table_name WHERE col2_name + col3_name >= val
     SELECT * FROM table_name -- * means all
     -- 3) update
-    UPDATE table_name SET col1=val1, col2=val2 WHERE col3=val3;
+    UPDATE table_name SET col1=val1, col2=val2 WHERE cond;
     -- WHERE controls its sql statement based on the bool result of the cond after it
     -- 4) delete
-    DELETE FROM table_name WHERE condition;
+    DELETE FROM table_name WHERE cond;
 
 -- DQL (query)
     -- ?
@@ -124,6 +125,57 @@
     -- referential integrity (rules for foreign key)
 
 
+-- Sorting (ORDER BY clause)
+----------------------------
+    SELECT col_name FROM table_name ORDER BY col_name;
+    SELECT col1_name, col2_name FROM table_name ORDER BY col_name ASC;
+    SELECT * FROM table_name ORDER BY col_name DESC;
+    SELECT * FROM table_name ORDER BY col1_name ASC, col2_name DESC;
+      -- ASC initial to final | DESC final to initial
+      -- if not provided ASC | DESC it's ASC by default
+      -- sorting for str vals is as alphabetical order
+      -- when sorting with 2 cols: 1st col has priority then excutes 2nd if 1st col vals are the same
+
+
+-- Filteration (WHERE clause)
+----------------------------- 
+  SELECT * FROM table_name WHERE logic_op;
+    -- % in a string form while using WHERE clauses is a wild card (means IDC how many chars)
+    ALL
+      -- compare a single val to all the vals in another val set.
+    ANY
+      -- compare a val to any applicable val in the list as per the cond.
+    BETWEEN AND
+      -- search for vals that are within a set of vals, given the min val and the max val.
+    EXISTS
+      -- search for the presence of a row in a specified table that meets a certain criterion.
+    IN (val1, val2)
+      -- compare a val to a list of literal vals that have been specified.
+    LIKE "%"
+      -- compare a val to similar vals using wildcard operators.
+    IS NULL
+      -- Used to compare a value with a NULL value.
+    UNIQUE
+      -- Searches every row of a specified table for uniqueness (no duplicates).
+    NOT
+      -- Reverses the meaning of the logical operator with which it is used.
+      -- For example: NOT EXISTS, NOT BETWEEN, NOT IN, NOT NULL, etc. This is a negate operator.
+    cond1 AND cond2 AND cond3
+    cond1 OR cond2 OR cond3
+    cond1 AND (cond2 OR cond3)
+      -- for multiple conds in WHERE clause.
+
+
+-- DISTINCT clause
+------------------
+    SELECT DISTINCT col_name FROM tbl_name;
+    SELECT count(DISTINCT col1_name, col2_name) FROM tbl_name
+      -- For one col or expression is provided, the query will return the unique vals for that col. 
+      -- For more than one col or expression, the query will retrieve unique combinations for those cols.
+      -- doesn't ignore NULL vals as they are considered as unique vals by DISTINCT.
+      -- can also be used with SQL aggregate funcs like COUNT, AVG, MAX
+
+
 -- Notes
 --------
 -- db_name must be unique and has max of 63 chars
@@ -138,38 +190,54 @@
     -- table=entities (col=field, row=record, cell, attr, datatypes-values-domain)
     -- key=attr (candidate=unique=primary+(alternate=secondary), composite, foreign=ref, relations(121-12many-many2many))
     -- CRUD (create(INSERT) - read=retrieve(SELECT) - UPDATE - DELETE)
+    -- sorting (ascending, descending)
     -- user (privileges, permissions)
     -- datatypes (miscellaneous, CLOB, BLOB, binary, encoding)
-    -- constraints=rules, Integrity, conds
+    -- constraints=rules, Integrity
+    -- keywords, clause
+    -- conds, arithmetic & logic+Comparison, operation, operator, operand, modulus
     -- 
 
 
 -- keywords
 -----------
-    WHERE LIKE
-    NULL
-    NOT NULL
-    DEFAULT
-    = "" ( , ) ;
-    
     + - * / %
     SELECT arith_op
 
-    CREATE DATABASE
-    CREATE TABLE
+    = > >= < <= <> != !> !<
+    SELECT FROM WHERE logic_cond
+
+    CREATE DATABASE db_name;
+    CREATE TABLE tbl_name;
+
     SHOW DATABASES;
     SHOW TABLES;
-    SHOW COLUMNS FROM
-    ALTER TABLE ADD
-    ALTER TABLE MODIFY COLUMN
-    ALTER TABLE DROP
-    DROP DATABASE
-    DROP TABLE
+    SHOW COLUMNS FROM tbl_name;
 
-    INSERT INTO VALUES
-    INSERT INTO SELECT FROM
-    SELECT FROM WHERE
-    UPDATE SET WHERE
-    DELETE FROM WHERE
+    ALTER TABLE tbl_name ADD (col1_name datatype1, col2_name datatype2);
+    ALTER TABLE tbl_name MODIFY (col1_name datatype1, col2_name datatype2);
+    ALTER TABLE tdl_name DROP COLUMN col_name;
+    -- ALTER TABLE tdl_name DROP COLUMNS col1_name, col2_name;
 
-    --
+    DROP DATABASE db_name;
+    DROP TABLE tbl_name;
+    TRUNCATE TABLE tbl_name;
+
+    INSERT INTO tbl_name(col1_name datatype1, col2_name datatype2) VALUES (val11, val21), (val12, val22);
+    INSERT INTO tbl1_name SELECT col_name FROM tbl2_name;
+
+    SELECT DISTINCT col_name FROM tbl_name;
+    SELECT * FROM tbl_name WHERE logic_op;
+
+    SELECT arith_op;
+    SELECT col1_name + col2_name FROM tbl_name;
+    SELECT col1_name / col2_name FROM tbl_name WHERE col3_name - col4_name <> val;
+
+    SELECT col_name FROM tbl_name ORDER BY col_name;
+    =
+    SELECT col_name FROM tbl_name ORDER BY col_name ASC;
+    SELECT col1_name FROM tbl_name ORDER BY col2_name DESC;
+    SELECT col1_name FROM tbl_name ORDER BY col2_name ASC, col3_name DESC;
+    
+    UPDATE tbl_name SET col1_name = val1, col2_name = val2 WHERE col3_name LIKE "Val%";
+    DELETE * FROM tbl_name WHERE col_name BETWEEN val1 AND val2;
