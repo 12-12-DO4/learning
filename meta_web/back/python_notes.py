4d11303ee97615710267fb0c19d65c4d4c2d4912
12-12-DO4 <mustafayasser031212@gmail.com>
2023-07-15 09:30:29 +0300

    some notes on py

---
+++ meta_web/back/python_notes.py
@@ -0,0 +1,76 @@
+# print(type(oct(10)))
+# print(type(hex(10)))
+# print()
+
+# print(10//4)
+# print(10/4)
+# print()
+
+# for x in range(3, 6):
+#   print(x, end=" ")
+# print()
+
+# print("DO4"[2] * 2)
+# print()
+
+
+
+'''
+  this is a docstring
+  multiline comment
+
+
+websites
+========
+1) https://www.python.org/
+    https://pypi.org/
+    https://docs.python.org/3/
+
+2) https://realpython.com/
+
+3) https://www.w3schools.com/python/default.asp
+
+
+Focus
+=====
+pure functions
+Data Structure
+Algorithims (3)
+Big O notation (7)
+Comprehensions
+zip(iter1, iter2, iter3, ...)
+classes(
+  exploration (dir, declaration, print)
+  inheretance (multiple, multi-level, hierarchical, hybrid)
+  mro(), help(), Algorithims(DFS, C3)
+  built-in functions (isinstance, issubclass, super)
+  abstract classes, methods and objects
+)
+modules, libraries, third-party packages & pip & flags
+scopes (LEGB + nonlocal)
+testing (unit-testing, TDD, markers)
+
+
+Notes
+=====
+if arguments of zip() function don't have the same length it will result in with the shorter length
+
+
+class should be pre declared before instantiation
+c=B() #this will give error
+class B:
+  b=5
+b=B() #this will work and instantiate
+
+
+Order of classes in multiple inheretance is important
+
+
+remember that super built-in function shares variables and methods in paranet and sibling classes (classes with the same parent class)
+
+
+statements inside a class body get executed irrespective of the instance creation. If a print statement inside the constructor, is not executed that's because it's inside a function. 
+
+
+
+'''
