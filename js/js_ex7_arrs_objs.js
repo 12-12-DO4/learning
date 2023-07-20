522dd7ae49c12d59378f890d063c5029d0f5eea4
12-12-DO4 <mustafayasser031212@gmail.com>
2023-07-20 14:05:54 +0300

    week2 js exercises

---
+++ js/js_ex7_arrs_objs.js
@@ -0,0 +1,10 @@
+var clothes = [];
+clothes.push("skirt", "pants", "socks", "shirt", "jacket");
+clothes.pop();
+clothes.push("t-shirt");
+console.log(clothes[2]);
+
+var favCar = {};
+favCar.color = "black";
+favCar.covertible = true;
+console.log(favCar);
