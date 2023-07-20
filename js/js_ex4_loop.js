4bf7e61350cc38d2f05bc8f8969f8225c46232b6
12-12-DO4 <mustafayasser031212@gmail.com>
2023-07-20 10:29:31 +0300

    week1 js exercieses

---
+++ js/js_ex4_loop.js
@@ -0,0 +1,33 @@
+for (var i=1; i<=5; i++) {
+  console.log(i);
+}
+console.log('Counting completed!');
+
+
+for (var i=5; i>0; i--) {
+  console.log(i);
+}
+console.log('Countdown finished!');
+
+
+var i = 0;
+while (i<5) {
+  i++;
+  console.log(i);
+}
+console.log('Counting completed!');
+
+
+i = 5;
+while (i>0) {
+  console.log(i);
+  i = i-1;
+}
+console.log('Countdown finished!');
+
+
+var year = 2018;
+while (year<2023) {
+  console.log(year);
+  year++;
+}
