e7e5d26843757812de97963b1a7108699e0a10bf
12-12-DO4 <mustafayasser031212@gmail.com>
2023-07-25 23:20:18 +0300

    lab 3 in react

--- meta_labs/front/first-react-comp/src/App.js
+++ meta_labs/front/first-react-comp/src/App.js
@@ -1,11 +1,12 @@
 import Heading from "./Heading";
 
 function App() {
-  return (
-    <div className="App">
-      <Heading />
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <Heading firstName="Bob" />
+            <Heading firstName="DO4" />
+        </div>
+    );
+};
 
 export default App;
--- meta_labs/front/first-react-comp/src/Heading.js
+++ meta_labs/front/first-react-comp/src/Heading.js
@@ -1,7 +1,7 @@
-function Heading() {
-  return (
-    <h1></h1>
-  )
+function Heading(props) {
+    return (
+        <h1>Hello, { props.firstName }</h1>
+    )
 }
 
 export default Heading;
