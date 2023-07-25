208928e3918adfd32533ece9f69adb685e440922
12-12-DO4 <mustafayasser031212@gmail.com>
2023-07-25 23:23:00 +0300

    lab 4 in react

---
+++ meta_labs/front/multiple-comp/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div>
+      <h1>Task: Add three Card elements</h1>
+      <Card h2="First Card's h2" h3="First Card's h3" />
+      <Card h2="Second Card's h2" h3="Second Card's h3" />
+      <Card h2="Third Card's h2" h3="Third Card's h3" />
+    </div>
+  );
+}
+
+export default App;
