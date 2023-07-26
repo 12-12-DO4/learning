e5519740a71588a042250bbf7d01325889d43e1d
12-12-DO4 <mustafayasser031212@gmail.com>
2023-07-26 17:45:00 +0300

    lab 5 & 6 in react

---
+++ meta_labs/front/manage-states/src/Fruits.js
@@ -0,0 +1,11 @@
+const Fruits = (props) => {
+  return (
+    <div>
+      {props.fruits.map((f) => (
+        <p key={f.id}>{f.fruitName}</p>
+      ))}
+    </div>
+  );
+};
+
+export default Fruits;
