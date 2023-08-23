d9df47457f4182ceada548ba860e80e50522e11d
Moustafa Yasser <135194370+12-12-DO4@users.noreply.github.com>
2023-08-23 07:16:58 +0300

    Delete meta_web directory

--- meta_web/front/base/js/js_note.test.js
+++
@@ -1,5 +0,0 @@
-const toTest = require('./js_notes')
-
-test('testing the func', () => {
-  expect(toTest()).toBe(true)
-});
--- meta_web/front/react/firstapp/src/Home.js
+++
@@ -1,5 +0,0 @@
-const Home = () => {
-  return <h1>Hello from main page</h1>
-}
-
-export default Home;
